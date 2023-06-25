fbea437c30a95a4686841a28450e37a8a5fe7811
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2023-06-25 15:25:31 +0100

    mostly finish first bit

--- godot4-tutorial-daynightcycle/src/scenes/01-setup.tsx
+++ godot4-tutorial-daynightcycle/src/scenes/01-setup.tsx
@@ -1,6 +1,6 @@
-import {Grid, Layout, makeScene2D, Line, Txt, Circle} from '@motion-canvas/2d/lib';
+import {Grid, Layout, makeScene2D, Line, Txt, Circle, Gradient} from '@motion-canvas/2d/lib';
 import {createRef, createSignal, PossibleVector2, all, chain, Color, waitUntil, useDuration, waitFor} from '@motion-canvas/core';
-
+import {CodeBlock, insert, edit} from '@motion-canvas/2d/lib/components/CodeBlock';
 
 export default makeScene2D(function* (view) {
 
@@ -13,6 +13,7 @@ export default makeScene2D(function* (view) {
   const xAxisLabel = createRef<Txt>();
   const yAxisLabel = createRef<Txt>();
   const lineFunc = createRef<Line>();
+  const yValueBoundaries = createRef<Line>();
 
   // Create point objects
   const pointSamples = 1000;
@@ -27,6 +28,7 @@ export default makeScene2D(function* (view) {
   // Text settings
   const fontFamily = 'Cascadia Mono';
   const fontWeight = 1000;
+  const formula = createRef<CodeBlock>();
 
   // Graph options
   const xAxisWidth = 1400;
@@ -34,8 +36,38 @@ export default makeScene2D(function* (view) {
   const altitude = createSignal(0.0);
   const pointOffsetY = createSignal(0.0);
   const maxAltitude = 350;
-  const graphColor = Color.createSignal('#DAE4ED');
-  const lineStroke = Color.createSignal('#00c25b00');
+  const graphXAlpha = createSignal(0);
+  const graphYAlpha = createSignal(0);
+  const graphXLabelAlpha = createSignal(0);
+  const graphYLabelAlpha = createSignal(0);
+  const graphXLength = createSignal(0);
+  const graphYLength = createSignal(0);
+  const graphXColor = () => new Color('#dae4ed00').alpha(graphXAlpha());
+  const graphYColor = () => new Color('#dae4ed00').alpha(graphYAlpha());
+  const graphXLabelColor = () => new Color('#dae4ed00').alpha(graphXLabelAlpha());
+  const graphYLabelColor = () => new Color('#dae4ed00').alpha(graphYLabelAlpha());
+  const gradientAlpha = createSignal(0);
+  const yBoundaryPercentage = createSignal(0.0);
+
+  const gradient = new Gradient({
+    type: 'linear',
+    from: () => [0, 400 + pointOffsetY()],
+    to: () => [0, -300 + pointOffsetY()],
+    stops: [
+      {offset: 0, color: () => new Color('#2b2e55').alpha(gradientAlpha())}, // night
+      {offset: 0.3, color: () => new Color('#6d3190').alpha(gradientAlpha())}, // dusk
+      {offset: 0.5, color: () => new Color('#f375ab').alpha(gradientAlpha())}, // sunrise1
+      {offset: 0.75, color: () => new Color('#ff989b').alpha(gradientAlpha())}, // sunrise2
+      {offset: 0.90, color: () => new Color('#ffdeddfd').alpha(gradientAlpha())}, // morning 
+      {offset: 1.0, color: () => new Color('#ffffff').alpha(gradientAlpha())}, // day 
+    ],
+  });
+
+  // Point options
+  const pointAlpha = createSignal(0.0);
+  const pointColor = () => new Color('#ffffff').alpha(pointAlpha());
+  const pointPercentageZero = 0.042;
+  const pointPercentage = createSignal(pointPercentageZero);
 
   const lineFuncPoints = createSignal(() => {
     // recalculate point positions
@@ -52,9 +84,9 @@ export default makeScene2D(function* (view) {
         width={9000}
         height={7000}
         stroke={gridColor}
-        lineWidth={6}
+        lineWidth={8}
         spacing={250.0}
-        x={1000}
+        x={1000 - 680}
       />
       <Grid
         ref={grid_large}
@@ -63,22 +95,22 @@ export default makeScene2D(function* (view) {
         stroke={'#252a31'}
         lineWidth={6}
         spacing={4 * 250.0}
-        x={1000}
+        x={1000  - 680}
       />
       <Layout x={-680}>
         <Circle // graph center
-          fill={graphColor}
+          fill={graphXColor}
           closed={true}
           size={25}
         />
         <Line // x axis
           ref={xAxis}
           lineWidth={10}
-          stroke={graphColor}
+          stroke={graphXColor}
           endArrow={true}
           points={() => [
             [0, 0],
-            [xAxisWidth, 0]
+            [xAxisWidth * graphXLength(), 0]
             ]}
         />
         <Txt // x axis label
@@ -87,18 +119,18 @@ export default makeScene2D(function* (view) {
           fontFamily={fontFamily}
           fontWeight={fontWeight}
           fontSize={60}
-          fill={graphColor}
+          fill={graphXLabelColor}
           x={xAxis().x() + xAxisWidth / 2.0}
           y={80}
         />
         <Line // y axis
           ref={yAxis}
           lineWidth={10}
-          stroke={graphColor}
+          stroke={graphYColor}
           endArrow={true}
           points={() => [
             [0, 0],
-            [0, -maxAltitude * 2 - 100.0]
+            [0, (-maxAltitude * 2 - 100.0) * graphYLength()]
             ]}
         />
         <Txt // y axis label
@@ -107,7 +139,7 @@ export default makeScene2D(function* (view) {
           fontFamily={fontFamily}
           fontWeight={fontWeight}
           fontSize={40}
-          fill={graphColor}
+          fill={graphYLabelColor}
           rotation={-90}
           x={-80}
           y={-maxAltitude - 50}
@@ -115,38 +147,102 @@ export default makeScene2D(function* (view) {
         <Line // line func
           ref={lineFunc}
           points={lineFuncPoints}
-          lineWidth={8}
-          stroke={lineStroke}
+          lineWidth={12}
+          stroke={gradient}
           x={-pointDistance * 5025.0 * 0.075}
         />
+        <Circle
+            fill={pointColor}
+            size={75}
+            position={() => lineFunc().getPointAtPercentage(pointPercentage()).position.sub([420, 0])}
+        />
+        <Line // y value between 0 and 1 indicator
+           ref={yValueBoundaries}
+           lineWidth={20}
+           startArrow={true}
+           endArrow={true}
+           lineDash={[25, 25]}
+           points={() => [
+            [500, 0],
+            [500, yBoundaryPercentage() * (-maxAltitude * 2 - 100.0) * graphYLength()]
+            ]}
+           stroke={'#50BD6A'}
+        />
       </Layout>
+      <CodeBlock
+        ref={formula}
+        fontFamily={fontFamily}
+        fontSize={120}
+        language='gdscript'
+        y={580}
+      />
     </Layout>
    );
 
 
     /*
       * Let us visualize the 'intensity' of the night for a moment.
-      * The x-axis represents time: 0 is full night, 1 is full day.
+      * The x-axis represents time.
       */
     const setupDuration = useDuration('setup');
     yield* all(
       layout().scale(1.2, setupDuration),
-      gridColor('#252a31ff', setupDuration)
+      gridColor('#252a31ff', setupDuration),
+      graphXAlpha(0.2, setupDuration),
+      graphYAlpha(1.0, setupDuration),
+      graphYLength(1.0, setupDuration)
     );
-    yield* lineStroke('#00c25bff', 1.0)
-    yield* pointOffsetY(0, useDuration('intro-full-night'));
+    
+    yield* graphYLabelAlpha(1.0, useDuration('gradient-y-alpha'));
+    
+    const introXAxisDuration = useDuration('intro-x-axis');
+    yield* all(
+      graphXAlpha(1.0, introXAxisDuration),
+      graphXLength(1.0, introXAxisDuration)
+    );
+    const gradientXAlpha = useDuration('gradient-x-alpha');
+    yield* all(
+      graphXLabelAlpha(1.0, gradientXAlpha),
+    );
+
+    
+
+   /* 0 is full night, 1 is full day. */
+   const fullNightDuration = useDuration('intro-full-night');
+
+   yield* all(
+       gradientAlpha(1.0, fullNightDuration),
+       pointOffsetY(0, fullNightDuration),
+    );
+
     yield* pointOffsetY(-maxAltitude * 2, useDuration('intro-full-day'))
 
-    /* We can use a sine function to transition smoothly between these values.*/
+    yield* pointOffsetY(0, useDuration('reset-to-0'))
 
-    yield* pointOffsetY(0, 1)
-
+    const useSineFunctionDuration =  useDuration('use-sine-function');
     yield* all(
-      altitude(maxAltitude, 3)
+      altitude(maxAltitude, useSineFunctionDuration),
+      pointAlpha(1.0, useSineFunctionDuration)
     );
 
+    const transitionSmoothlyDuration = useDuration('transition-smoothly');
+    yield* all(
+      pointPercentage(0.2, transitionSmoothlyDuration)
+    );
 
-    yield* waitFor(5);
+    const cleanupTransitionSmoothlyDuration = useDuration('transition-cleanup');
+    yield* all(
+      pointAlpha(0.0, cleanupTransitionSmoothlyDuration),
+      gradientAlpha(0.3, cleanupTransitionSmoothlyDuration)
+    );
+
+    yield* yBoundaryPercentage(1.0, useDuration('y-value-between-0-1'))
+
+    const hideYValueBetween = useDuration('hide-y-value-between');
+    yield* all(
+      yBoundaryPercentage(0.0, hideYValueBetween),
+      gradientAlpha(1.0, hideYValueBetween)
+    )
 
     /* Since the y value should always be between 0 and 1 we need to move the entire function up
      * by 1 unit and divide it by 2.
@@ -159,7 +255,18 @@ export default makeScene2D(function* (view) {
      * start at dusk and gradually transition to day.
      */
 
-    yield* waitFor(9.0);
+    pointPercentage(pointPercentageZero);
+    const pointFadeIn = useDuration('point-at-time-0');
+    yield* all(
+      pointAlpha(1.0, pointFadeIn)
+    );
+
+    const graduallyTransitionToDayDuration = useDuration('gradually-transition-to-day');
+    yield* all(
+      pointPercentage(0.063, graduallyTransitionToDayDuration)
+    );
+
+    yield* pointAlpha(0.0, 1.0);
 
 
     /*If you want to adjust the starting time, simply shift the graph along the x-axis.*/

be017297ac4d8fde65ad77ed5db977ab816f574f
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2023-06-27 20:00:37 +0100

    emit signal when changed animation

--- godot4-tutorial-daynightcycle/src/scenes/07-emit-signal-only-when-changed.tsx
+++ godot4-tutorial-daynightcycle/src/scenes/07-emit-signal-only-when-changed.tsx
@@ -1,5 +1,5 @@
-import {Grid, Layout, makeScene2D, Line, Txt} from '@motion-canvas/2d/lib';
-import {waitFor} from '@motion-canvas/core';
+import {Grid, Layout, makeScene2D, Line, Txt, Rect} from '@motion-canvas/2d/lib';
+import {all, range, makeRef, createRef, linear, delay} from '@motion-canvas/core';
 
 export default makeScene2D(function* (view) {
 
@@ -9,12 +9,67 @@ export default makeScene2D(function* (view) {
     * not every single frame. 
     */
 
+   const rects: Rect[] = [];
+   const rectBgs: Rect[] = [];
+   const txts: Txt[] = [];
+   const fontFamily = 'Cascadia Mono';
+   const fontWeight = 1000;
+
+   const layout = createRef<Layout>();
+
    view.add(
-    <Layout>
-        <Txt
-           text={"Emit only when changed"}
-        />
+    <Layout ref={layout} layout gap={590} alignItems="center" y={60}>
+      {range(80).map(i => (
+        <Layout layout direction="column" gap={150}>
+            <Rect
+            ref={makeRef(rects, i)}
+            radius={185}
+            width={480}
+            height={480}
+            fill={'#DAE4ED'}
+            >
+                
+            <Rect
+            ref={makeRef(rectBgs, i)}
+            radius={185}
+            width={480}
+            height={480}
+            fill={'#00FFA3'}
+            opacity={0.0}
+            />
+            </Rect>
+            
+            <Txt
+                ref={makeRef(txts, i)}
+                fontFamily={fontFamily}
+                fontWeight={fontWeight}
+                text={'Frame ' + i}
+                textAlign={'center'}
+                width={500}
+                fontSize={150}
+                fill={'#DAE4ED'}
+                
+            />
+        </Layout>
+      ))}
     </Layout>);
 
-    yield* waitFor(9.5);
+    layout().x(14000);
+
+    yield* all(
+        layout().x(12000, 10, linear),
+        delay(5, rects[27].fill('#00FFA3', 1.0)),
+        delay(5, txts[27].fill('#00FFA3', 1.0)),
+        delay(5, rectBgs[27].opacity(1.0, 0.25)),
+        delay(5, rectBgs[27].scale(1.5, 0.25)),
+        delay(5.1, rectBgs[27].opacity(0.0, 0.3)),
+        delay(5, txts[27].fill('#00FFA3', 1.0)),
+        delay(7, rects[29].fill('#00FFA3', 1.0)),
+        delay(7, txts[29].fill('#00FFA3', 1.0)),
+        
+        delay(7, rectBgs[29].opacity(1.0, 0.25)),
+        delay(7, rectBgs[29].scale(1.5, 0.25)),
+        delay(7.1, rectBgs[29].opacity(0.0, 0.3)),
+    );
+
 });
